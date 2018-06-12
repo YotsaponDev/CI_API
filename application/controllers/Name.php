a9bb6bf09fb1d99b95b9780771774b0964b403d8
yotsapon6 <boss.yotsapon@gmail.com>
2018-06-12 23:48:12 +0700

    CORS Allow For Cross URL

--- application/controllers/Name.php
+++ application/controllers/Name.php
@@ -1,5 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, XKMS,delayi");
 
 class Name extends CI_Controller {
 	public function __construct(){
